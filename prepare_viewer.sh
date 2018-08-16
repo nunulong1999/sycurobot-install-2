df9cf839b2ea3437cd97d1253242c6facc068266
Steve Nguyen <steve.nguyen.000@gmail.com>
2018-08-16 04:26:07 +0200

    add a script to compile the 3D viewer

---
+++ prepare_viewer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+sudo apt-get install freeglut3-dev libx11-dev libxrandr-dev libfreetype6-dev
+./workspace install rhobandeps/sfml
+cd src/rhoban/model
+git fetch --tags
+git checkout final_2018
+cd ../../..
+cd src/rhoban/rhio/
+git pull
+git checkout final_2018
+cd ../../..
+catkin config --profile novision -a --cmake-args -DBUILD_ROBOCUP_VIEWER=ON
